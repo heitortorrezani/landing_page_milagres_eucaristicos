680f9ac19ffb419bf9ab0c2e7a88a797d9e745e8
heitortorrezani <49353207+heitortorrezani@users.noreply.github.com>
2025-10-18 12:22:29 -0300

    Separação back/frontEnd

--- lib/Pages/autor/global_author.dart
+++
@@ -1,5 +0,0 @@
-final String textAuthor = 
-'Dr. Franco Serafini é cardiologista italiano e pesquisador católico. Tornou-se conhecido por investigar com rigor científico os milagres eucarísticos reconhecidos pela Igreja Católica, trazendo uma perspectiva única que une a medicina moderna com a fé cristã.';
-
-
-late double widthW;
--- lib/Pages/hero/hero_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:url_launcher/url_launcher.dart';
-String url = '';
-
-Future<void> OpenUrl(url) async {
-  final Uri uri = Uri.parse(url);
-  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
-    throw Exception('Não foi possível abrir $url');
-  }
-}
