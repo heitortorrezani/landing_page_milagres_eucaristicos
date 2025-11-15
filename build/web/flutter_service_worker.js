e947de5379b229584266233a19703d165139815d
heitortorrezani <49353207+heitortorrezani@users.noreply.github.com>
2025-11-15 17:35:10 -0300

    subindo novo ip do servidor())

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f0559e3c408404e0c2db1e7e2b0dc414
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "814fa658005fa201cdc972d9698fa9e2",
+"flutter_bootstrap.js": "d542dcbc688d53dbf05e46f91875595b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
